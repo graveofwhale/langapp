71dc8d17816959bf42faf2a2930b09b75d09abf7
fractalMinder <graveofwhale@gmail.com>
2025-04-22 14:40:17 +0900

    @@1

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+  align-items: center;
+  justify-content:center;
+`;
+const Title = styled.Text`
+  font-size: 40px
+`;
 
 export default function App() {
+  const [y, setY] = useState(0)
+  const [intervalId, setIntervalId] = useState(null);
+  const moveUp = () => {
+    const id = setInterval(() => setY(prev => prev + 1), 10)
+    setIntervalId(id);
+  }
+  console.log("rendering")
+  useEffect(() => {
+    if (y === 200) {
+      clearInterval()
+    }
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Container>
+      <Box onPress={moveUp} style={{
+        transform: [{ translateY: y },],
+      }} />
+    </Container>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
