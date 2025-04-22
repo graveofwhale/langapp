fcb6fffba9944d5ac8874b36a95c9d661edc816d
fractalMinder <graveofwhale@gmail.com>
2025-04-22 16:40:03 +0900

    Animated component 에 대한 이해

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 
@@ -11,35 +11,40 @@ const Container = styled.View`
   justify-content: center;
 `;
 
-const Box = styled.TouchableOpacity`
+const Box = styled.View`
   background-color: tomato;
   width: 200px;
-  height: 200px;
-  align-items: center;
-  justify-content:center;
-`;
-const Title = styled.Text`
-  font-size: 40px
+  height: 200px;  
 `;
 
+const AnimatedBox = Animated.createAnimatedComponent(Box); // 이 편이 import가 적다
+//const Title = styled.Animated.View``;
+
 export default function App() {
-  const [y, setY] = useState(0)
-  const [intervalId, setIntervalId] = useState(null);
+  const [up, setUp] = useState(false);
+  const toggleUp = () => setUp((prev) => !prev)
+  const Y = new Animated.Value(0);
+  //useState를 쓰지마라! 값을 줄땐 animated api를 이용, animate 컴포넌트는 따로 존재한다.
   const moveUp = () => {
-    const id = setInterval(() => setY(prev => prev + 1), 10)
-    setIntervalId(id);
+
+    Animated.spring(Y, {
+      toValue: up ? 200 : -200,
+      // tension: 555,
+      // friction: 1, ///마찰력
+      useNativeDriver: true, // nativeDriver를 사용해야 애니메이션을 시작하기전 모든 것을 네이티브로 전송
+    }).start(toggleUp) //애니메이션 토글업 함수가 실행되면서 스테이트값을 건드리고 이후 리렌더링이 일어난다. 그러면서 애니메이트 값이 다시 0으로 
   }
-  console.log("rendering")
-  useEffect(() => {
-    if (y === 200) {
-      clearInterval(intervalId)
-    }
-  })
+
+  console.log(Y)
+  console.log('rendering')
+  Y.addListener(() => console.log(Y))
   return (
     <Container>
-      <Box onPress={moveUp} style={{
-        transform: [{ translateY: y },],
-      }} />
+      <TouchableOpacity onPress={moveUp} >
+        <AnimatedBox style={{
+          transform: [{ translateY: Y },],
+        }} />
+      </TouchableOpacity>
     </Container>
   )
 }
